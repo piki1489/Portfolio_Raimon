0f3b88407b5246ec1546b89a3f99f49200c62fd4
piki1489 <piki1489@hotmail.com>
2023-01-16 19:46:08 +0100

    lineas separando divs terminadas

--- portfolio_raimon/src/app/app-routing.module.ts
+++ portfolio_raimon/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 import { OverviewComponent } from './components/overview/overview.component';
 
 const routes: Routes = [
-  { path: '/overview', component: OverviewComponent }
+  { path: '/overview', component: OverviewComponent },
+  { path: '/overview', component: FooterComponent }
 ];
 
 @NgModule({
--- portfolio_raimon/src/app/app.module.ts
+++ portfolio_raimon/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OverviewComponent } from './components/overview/overview.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OverviewComponent
+    OverviewComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
